3d120af41dbd4c90d5007f71509f166f5ee8b744
ScopeGaming <55145930+ScopeGaming@users.noreply.github.com>
2019-10-01 10:17:22 -0400

    Add files via upload

---
+++ Morality Project.py
@@ -0,0 +1,112 @@
+import numpy as np
+
+def Question1():
+
+    Option1A = ["Tell you Sister and ruin the weding?"]
+    Option1B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option1A) )
+    print ( str(Option1B) )
+
+def Question2 ():
+
+    Option2A = ["Turn the robber in to the police?"]
+    Option2B = ["Say nothing because the money wen tot a good cause?"]
+    
+
+    print ( " You are an eyewitness to a crime: A man has robbed a bank, but instead of keeping the money for himself, he donates it to a poor orphanage that can now afford to feed, clothe, and care for its children. You know who committed the crime. If you go to the authorities with the information, there's a good chance the money will be returned to the bank, leaving a lot of kids in need. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option2A) )
+    print ( str(Option2B) )
+
+def Question3 ():
+
+    Option3A = ["Tell you Sister and ruin the weding?"]
+    Option3B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option3A) )
+    print ( str(Option3B) )
+
+def Question4 ():
+
+    Option4A = ["Tell you Sister and ruin the weding?"]
+    Option4B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option4A) )
+    print ( str(Option4B) )
+
+def Question5 ():
+
+    Option5A = ["Tell you Sister and ruin the weding?"]
+    Option5B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option5A) )
+    print ( str(Option5B) )
+
+def Question6 ():
+    Option6A = ["Tell you Sister and ruin the weding?"]
+    Option6B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option6A) )
+    print ( str(Option6B) )
+
+def Question7 ():
+    
+    Option7A = ["Tell you Sister and ruin the weding?"]
+    Option7B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option7A) )
+    print ( str(Option7B) )
+
+def Question8 ():
+    
+    Option8A = ["Tell you Sister and ruin the weding?"]
+    Option8B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option8A) )
+    print ( str(Option8B) )
+
+def Question9 ():
+
+    Option9A = ["Tell you Sister and ruin the weding?"]
+    Option9B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option9A) )
+    print ( str(Option9B) )
+
+def Question10 ():
+    Option10A = ["Tell you Sister and ruin the weding?"]
+    Option10B = ["Let the wedding continue?"]
+    
+
+    print ( " You are at your sisters wedding an hour before the ceremony is to start. Earlier that day, you came across definitive proof that your sisters husband-to-be is having an affair with the best man, and you catch them sneaking out of a room together looking disheveled. If you tell your sister about the affair, her day will be ruined, but you don't want your sister to marry a cheater. What do you do? " )
+    print ( " Do you chose Option 1 or Option 2 " )
+    print ( str(Option10A) )
+    print ( str(Option10B) )
+
+print ("Created by: 0100000101101001011001000110010101101110001000000101001100101110")
+
